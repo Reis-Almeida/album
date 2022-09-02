8c0690bd97b3f9418b1acd9aa6c3b53ccf62b6e1
Reis-Almeida <84649246+Reis-Almeida@users.noreply.github.com>
2022-09-02 08:36:43 -0300

    corrigido 13

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,9 +11,40 @@ import { DatabaseProps } from '../types'
 
 const Home: NextPage = () => {
 
+  let images:DatabaseProps[] = Database
+  const query:string = useRouter().query?.search as string
+
+  const search = (data:string, query:string) => {
+    if(data.toLowerCase().search(query) !== -1) {
+        return true
+      }
+      return false
+    }
+
+    if(query != undefined) {
+        images = Database.filter((data:DatabaseProps) => {
+
+        if(search(data.title, query) || search(data.text, query)) {
+            return data
+        }
+      })
+    }
+
   return (
     <StyledHome>
-      hellow
+      <Head>
+          <title>Home - Album do Conhecimento</title>
+      </Head>
+      <Search />
+      <div className="album">
+        {images.map((data:DatabaseProps) => (
+          <Link href={`/details/${data.id}`} key={`image${data.id}`}>
+            <a>
+              <Image src={data.image} objectFit="cover" layout="fill" alt="imagem do assunto" priority />
+            </a>
+          </Link>
+        ))}
+      </div>
     </StyledHome>
   )
 }
